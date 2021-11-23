eefa8f85db1d85b8ae45537f02190a7fa2ed30d3
Raj Kumar <interplaytheheads@gmail.com>
2021-11-23 15:26:28 +0200

    onclick events calling hello function

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,7 @@
         <h1>Hello From Home Component</h1>
         <h1>{{data}}</h1>
         <h1>{{msg}}</h1>
+        <button @click="hello"> Call Hello Function</button>
     </div>
 </template>
 <script>
@@ -13,6 +14,12 @@ export default{
     props:{
         data:String,
         msg: String,
+    },
+    methods: {
+        hello()
+        {
+            alert("Hello Function Called")
+        }
     }        
     }
 
