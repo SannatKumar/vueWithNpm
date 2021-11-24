425fba2c98fcb11c93b6995e247f6a584207f563
Raj Kumar <interplaytheheads@gmail.com>
2021-11-24 11:22:27 +0200

    My component created and exported

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="mycomponent"><h1>MYCOMPONENT</h1></div>
+</template>
+
+<script>
+export default{
+    mycomponent:"MyComponent"
+}
+</script>
+ 
+<style scoped>
+.mycomponent{
+    background-color:aquamarine;
+    height: 500px;
+}
+</style>
