6e61a4ea27cf9382710f1300283d65c46dd1e4f1
Raj Kumar <interplaytheheads@gmail.com>
2021-11-24 11:34:12 +0200

    Users Component created

---
+++ src/components/Users.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class = "users">
+        Users Component
+    </div> 
+</template>
+
+<script>
+export default{
+    users:"Users"
+
+}
+</script>
+
+<style scoped>
+.users{
+    background-color: burlywood;
+    height: 25px;
+}
+</style>
