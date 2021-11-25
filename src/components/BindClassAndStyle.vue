f19527093c5731342a80ac92940e68fafc8933c0
Raj Kumar <interplaytheheads@gmail.com>
2021-11-25 10:26:33 +0200

    bindclass and style properties added

---
+++ src/components/BindClassAndStyle.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1 class="vueStyle"> Class and Style Bindings</h1>
+        <h1 v-bind:style = "{color:'skyblue'}"> Style Bindings</h1>
+        <h1 v-bind:class = "{display: show}">Class bindings</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'BindClassAndStyle',
+    data(){
+        return{
+            message:"Hi"
+        }
+    }
+
+}
+</script>
+<style lang="css" scoped>
+.vueStyle{
+    color: orange;
+}
+</style>
+
