3e251f0caf4325a910c56ce0f0194d328d4ca68c
Raj Kumar <interplaytheheads@gmail.com>
2021-11-25 10:57:14 +0200

    new bindings added

--- src/components/BindClassAndStyle.vue
+++ src/components/BindClassAndStyle.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
         <h1 class="vueStyle"> Class and Style Bindings</h1>
-        <h1 v-bind:style = "{color:'skyblue'}"> Style Bindings</h1>
-        <h1 v-bind:class = "{display: show}">Class bindings</h1>
+        <h1 v-bind:style = "{color:colorName}"> Style Bindings</h1>
+        <h1 v-bind:class = "{display: show, vueStyle: show}">Class bindings</h1>
+        <h1 v-bind:style = "{color:colorName}"> Class With Objects</h1>
+        <h1 v-bind:class = "['display', 'vueStyle']">Class with Array</h1>
+        <h1 v-bind:class = "[d, v]">Class with variable assignment</h1>
     </div>
 </template>
 
@@ -11,7 +14,11 @@ export default{
     name: 'BindClassAndStyle',
     data(){
         return{
-            message:"Hi"
+            message:"Hi",
+            colorName: 'skyblue',
+            show: true,
+            d: 'display',
+            v: 'vueStyle'
         }
     }
 
@@ -19,7 +26,11 @@ export default{
 </script>
 <style lang="css" scoped>
 .vueStyle{
-    color: orange;
+    color: 'orange';
+}
+.display{
+    background-color: peru;
+    padding: 5px;
 }
 </style>
 
